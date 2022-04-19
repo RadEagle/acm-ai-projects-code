44a7b0581e921aa1d77f6553d5d3d3a96475938c
smuppalla <smuppalla521@ucla.edu>
2022-04-18 19:38:51 -0700

    Edited StartingDataset.py and starting_train.py

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -6,14 +6,31 @@ class StartingDataset(torch.utils.data.Dataset):
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    def __init__(self):
+    def __init__(self, csv_path, folder_path, img_size, transform=None):
+        df = pd.read_csv(csv_path)
+        self.img_size = img_size
+        self.img_ids = list(df['Image'])
+        for i, d in enumerate(self.image_ids):
+            self.image)ids[i] = folder_path + '/' + id
+        self.labels = list(df["Id"])
+        self.l = len(self.labels)
+        self.transform = transforms.Compose([
+            transforms.RandomCrop(self.img_size[0], padding=28),
+            transforms.RandomHorizontalFlip(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
         pass
 
     def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
+        #inputs = torch.zeros([3, 224, 224])
+        inputs = 0
+        with Image.open(self.image_ids[index]) as image:
+            inputs = transforms.functional.resize(image, self.img_size)
 
-        return inputs, label
+        inputs = transforms.ToTensor()(inputs)
+        inputs = self.transform(inputs)
+        
+        return inputs, self.labels[index]
 
     def __len__(self):
-        return 10000
+        return self.l
